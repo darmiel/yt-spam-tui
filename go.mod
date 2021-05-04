fac218360f1a8f36caa0498687b85264840d0e98
darmiel <71837281+darmiel@users.noreply.github.com>
2021-05-04 16:10:01 +0200

    ProgressBar Test

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/darmiel/yt-spam-tui
 go 1.16
 
 require (
-	github.com/charmbracelet/bubbletea v0.13.2 // indirect
-	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/charmbracelet/bubbles v0.7.9
+	github.com/charmbracelet/bubbletea v0.13.2
+	github.com/charmbracelet/lipgloss v0.2.1
 )
--- main.go
+++ main.go
@@ -2,100 +2,85 @@ package main
 
 import (
 	"fmt"
+	"github.com/charmbracelet/bubbles/progress"
+	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbletea"
-	"github.com/emirpasic/gods/maps/treemap"
+	"github.com/charmbracelet/lipgloss"
 	"log"
+	"time"
 )
 
 type model struct {
-	choices *treemap.Map
-	cursor  int
+	i       textinput.Model
+	p       *progress.Model
+	percent float64
 }
 
 var initialModel model
 
+type tickMsg time.Time
+
 func init() {
-	initialModel = model{
-		choices: treemap.NewWithStringComparator(),
+	i := textinput.NewModel()
+	i.CursorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("63"))
+	i.Width = 48
+	i.Placeholder = "Daniel"
+	i.CursorEnd()
+	i.Focus()
+
+	p, err := progress.NewModel(progress.WithScaledGradient("#FF7CCB", "#FDFF8C"))
+	if err != nil {
+		panic(err)
 	}
-	initialModel.choices.Put("Hallo", false)
-	initialModel.choices.Put("das", false)
-	initialModel.choices.Put("ist", false)
-	initialModel.choices.Put("ein", false)
-	initialModel.choices.Put("Test", false)
+
+	initialModel = model{i, p, 0.0}
 }
 
 func (model) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
+}
+
+func tickCmd() tea.Cmd {
+	return tea.Tick(time.Second/20, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEscape, tea.KeyEnter:
 			return m, tea.Quit
-		case "up":
-			if m.cursor > 0 {
-				m.cursor--
-			} else {
-				m.cursor = m.choices.Size() - 1
-			}
-			break
-		case "down":
-			if m.cursor < m.choices.Size()-1 {
-				m.cursor++
-			} else {
-				m.cursor = 0
-			}
-			break
-		case "enter", " ":
-			num := -1
-			m.choices.Each(func(key interface{}, value interface{}) {
-				check, ok := value.(bool)
-				if !ok {
-					return
-				}
-				num++
-				if num != m.cursor {
-					return
-				}
-				m.choices.Put(key, !check)
-			})
-			break
 		}
 		break
+
+	case tea.WindowSizeMsg:
+		m.p.Width = msg.Width - 4*2 - 4
+		return m, nil
+
+	case tickMsg:
+		m.percent += 1.0 / 20 * 2
+		if m.percent > 1.0 {
+			m.percent = 1.0
+			return m, tea.Quit
+		}
+		return m, tickCmd()
 	}
-	return m, nil
+	var cmd tea.Cmd
+	m.i, cmd = m.i.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
-	s := "Select:\n\n"
-
-	num := -1
-	m.choices.Each(func(key interface{}, value interface{}) {
-		choice, ok := key.(string)
-		if !ok {
-			return
-		}
-		check, ok := value.(bool)
-		if !ok {
-			return
-		}
-		num++
-		cursor := "  "
-		if m.cursor == num {
-			cursor = " >"
-		}
-
-		checked := "[ ]"
-		if check {
-			checked = "[x]"
-		}
-
-		s += fmt.Sprintf("%s %s :: (%v) %s\n", cursor, checked, num, choice)
-	})
-	return s
+	prc := float64(len(m.i.Value())) / 26.0
+	if prc > 1.0 {
+		prc = 1
+	}
+	return fmt.Sprintf("What's your name?\n\n%s\n\n  > [%s] <\n\n%s",
+		m.i.View(),
+		m.p.View(prc),
+		"(esc to quit)")
 }
 
 func main() {
