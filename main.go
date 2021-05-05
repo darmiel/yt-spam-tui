294556df4cb42b52170101f6b8518f055d673e90
darmiel <71837281+darmiel@users.noreply.github.com>
2021-05-05 21:33:37 +0200

    test log

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/rivo/tview"
+import (
+	"fmt"
+	"github.com/rivo/tview"
+	"time"
+)
 
 func main() {
 	prim := func(text string) tview.Primitive {
 		return tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(text)
 	}
+
+	logTxt := tview.NewTextView().
+		SetDynamicColors(true).
+		SetTextAlign(tview.AlignLeft)
+
+	w := tview.ANSIWriter(logTxt)
 
 	grid := tview.NewGrid().
 		SetRows(0, 10).
@@ -16,9 +26,23 @@ func main() {
 	grid.
 		AddItem(prim("Selected"), 0, 0, 1, 1, 0, 0, false).
 		AddItem(prim("Main-View"), 0, 1, 1, 1, 0, 0, false).
-		AddItem(prim("Log"), 1, 0, 1, 2, 0, 0, false)
+		AddItem(logTxt, 1, 0, 1, 2, 0, 0, false)
 
-	if err := tview.NewApplication().SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
+	app := tview.NewApplication().SetRoot(grid, true).SetFocus(grid)
+
+	go func() {
+		t := time.NewTicker(time.Second)
+		for {
+			select {
+			case d := <-t.C:
+				app.QueueUpdateDraw(func() {
+					_, _ = fmt.Fprintf(w, "[%s]: Hey! [red]what's up?[white]\n", d.Format("15:04:05"))
+				})
+			}
+		}
+	}()
+
+	if err := app.Run(); err != nil {
 		panic(err)
 	}
 }
